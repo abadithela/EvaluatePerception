389d483850f1355de45606032cb6d54a58f2ca4c
apurva <apurva@caltech.edu>
2023-08-19 15:30:06 -0700

    localization init

---
+++ packages/localization/src/abstract_state_publisher.py
@@ -0,0 +1,6 @@
+'''
+Script that publishes abstract position states of the robot. 
+The optitrack coordinates are converted to abstract state coordinates as 
+defined in coordinate_translator.py. Furthermore, velocity of the robot is also used
+to compute the abstract state of the robot
+'''
---
+++ packages/localization/src/vrpn_subscriber.py
@@ -0,0 +1,5 @@
+'''
+Script that subscribes to vrpn_client topics that publish opti-track 
+coordinates of the robots. These coordinates are converted to abstract state coordinates as 
+defined in coordinate_translator.py.
+'''
